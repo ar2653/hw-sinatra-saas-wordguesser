bfaa7a63944e8b3f419b9b46386bfa61dd649e29
ar2653 <ar2653@njit.edu>
2023-11-08 18:00:41 +0000

    Added logic for game and removed pending:true

--- app.rb
+++ app.rb
@@ -1,7 +1,81 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/flash'
+require './lib/wordguesser_game.rb'
 
-class MyApp < Sinatra::Base
-  get '/' do
-    "<!DOCTYPE html><html><head></head><body><h1>Hello World!!!</h1></body></html>"
+class WordGuesserApp < Sinatra::Base
+
+  enable :sessions
+  register Sinatra::Flash
+
+  before do
+    @game = session[:game] || WordGuesserGame.new('')
   end
+
+  after do
+    session[:game] = @game
+  end
+
+  # These two routes are good examples of Sinatra syntax
+  # to help you with the rest of the assignment
+  get '/' do
+    redirect '/new'
+  end
+
+  get '/new' do
+    erb :new
+  end
+
+  post '/create' do
+    # NOTE: don't change next line - it's needed by autograder!
+    word = params[:word] || WordGuesserGame.get_random_word
+    # NOTE: don't change previous line - it's needed by autograder!
+
+    @game = WordGuesserGame.new(word)
+    redirect '/show'
+  end
+
+  # Use existing methods in HangpersonGame to process a guess.
+  # If a guess is repeated, set flash[:message] to "You have already used that letter."
+  # If a guess is invalid, set flash[:message] to "Invalid guess."
+  post '/guess' do
+    begin
+      letter = params[:guess].to_s[0]
+      guesses_before_guess = @game.guesses
+      if !@game.guess(letter)
+        flash[:message] = "You have already used that letter."
+      elsif guesses_before_guess == @game.guesses
+        flash[:message] = "Invalid guess."
+      end
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
+    redirect '/show'
+  end
+
+  # Everytime a guess is made, we should eventually end up at this route.
+  # Use existing methods in WordguesserGame to check if player has
+  # won, lost, or neither, and take the appropriate action.
+  # Notice that the show.erb template expects to use the instance variables
+  # wrong_guesses and word_with_guesses from @game.
+  get '/show' do
+    case @game.check_win_or_lose
+    when :win
+      redirect '/win'
+    when :lose
+      redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
+  end
+
+  get '/win' do
+    redirect '/show' if @game.check_win_or_lose == :play
+    erb :win # You may change/remove this line
+  end
+
+  get '/lose' do
+    redirect '/show' if @game.check_win_or_lose == :play
+    erb :lose # You may change/remove this line
+  end
+
 end
--- spec/wordguesser_game_spec.rb
+++ spec/wordguesser_game_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 require 'wordguesser_game'
 
 describe WordGuesserGame do
-  # helper function: make several guesses
   def guess_several_letters(game, letters)
     letters.chars do |letter|
       game.guess(letter)
     end
   end
 
-  describe 'new', :pending => true do
-    it "takes a parameter and returns a WordGuesserGame object" do      
+  describe 'new' do
+    it "takes a parameter and returns a WordGuesserGame object" do
+      # Test the constructor to ensure it creates a WordGuesserGame
+      # object with the correct attributes.
       @game = WordGuesserGame.new('glorp')
       expect(@game).to be_an_instance_of(WordGuesserGame)
       expect(@game.word).to eq('glorp')
@@ -19,83 +20,96 @@ describe WordGuesserGame do
     end
   end
 
-  describe 'guessing', :pending => true do
+  describe 'guessing' do
     context 'correctly' do
       before :each do
+        # Test the game's handling of correct guesses and its return value.
         @game = WordGuesserGame.new('garply')
         @valid = @game.guess('a')
       end
-      it 'changes correct guess list', :pending => true do
+      it 'changes correct guess list' do
         expect(@game.guesses).to eq('a')
         expect(@game.wrong_guesses).to eq('')
       end
-      it 'returns true', :pending => true do
+      it 'returns true' do
         expect(@valid).not_to be false
       end
     end
+
     context 'incorrectly' do
       before :each do
+        # Test the game's handling of incorrect guesses and its return value.
         @game = WordGuesserGame.new('garply')
         @valid = @game.guess('z')
       end
-      it 'changes wrong guess list', :pending => true do
+      it 'changes wrong guess list' do
         expect(@game.guesses).to eq('')
         expect(@game.wrong_guesses).to eq('z')
       end
-      it 'returns true', :pending => true do
+      it 'returns true' do
         expect(@valid).not_to be false
       end
     end
+
     context 'same letter repeatedly' do
       before :each do
+        # Test the game's handling of repeated guesses of the same letter.
         @game = WordGuesserGame.new('garply')
         guess_several_letters(@game, 'aq')
       end
-      it 'does not change correct guess list', :pending => true do
+      it 'does not change correct guess list' do
+        # Check that repeated guesses of the same letter do not change the correct guess list.
         @game.guess('a')
         expect(@game.guesses).to eq('a')
       end
-      it 'does not change wrong guess list', :pending => true do
+      it 'does not change wrong guess list' do
+        # Check that repeated guesses of the same letter do not change the wrong guess list.
         @game.guess('q')
         expect(@game.wrong_guesses).to eq('q')
       end
-      it 'returns false', :pending => true do
+      it 'returns false' do
+        # Check that repeated guesses of the same letter return false.
         expect(@game.guess('a')).to be false
         expect(@game.guess('q')).to be false
       end
-      it 'is case insensitive', :pending => true do
+      it 'is case insensitive' do
+        # Check that the game is case-insensitive for guesses.
         expect(@game.guess('A')).to be false
         expect(@game.guess('Q')).to be false
         expect(@game.guesses).not_to include('A')
         expect(@game.wrong_guesses).not_to include('Q')
       end
     end
+
     context 'invalid' do
       before :each do
+      # Test the game's handling of invalid guesses.
         @game = WordGuesserGame.new('foobar')
       end
-      it 'throws an error when empty', :pending => true do
+      it 'throws an error when empty' do
         expect { @game.guess('') }.to raise_error(ArgumentError)
       end
-      it 'throws an error when not a letter', :pending => true do
+      it 'throws an error when not a letter' do
         expect { @game.guess('%') }.to raise_error(ArgumentError)
       end
-      it 'throws an error when nil', :pending => true do
+      it 'throws an error when nil' do
         expect { @game.guess(nil) }.to raise_error(ArgumentError)
       end
     end
   end
 
-  describe 'displayed word with guesses', :pending => true do
+  describe 'displayed word with guesses' do
     before :each do
+      # Test the game's word_with_guesses method.
       @game = WordGuesserGame.new('banana')
     end
-    # for a given set of guesses, what should the word look like?
+
     @test_cases = {
       'bn' =>  'b-n-n-',
       'def' => '------',
       'ban' => 'banana'
     }
+
     @test_cases.each_pair do |guesses, displayed|
       it "should be '#{displayed}' when guesses are '#{guesses}'" do
         guess_several_letters(@game, guesses)
@@ -106,17 +120,18 @@ describe WordGuesserGame do
 
   describe 'game status' do
     before :each do 
+      # Test the game's win, lose, and play status.
       @game = WordGuesserGame.new('dog')
     end
-    it 'should be win when all letters guessed', :pending => true do
+    it 'should be win when all letters guessed' do
       guess_several_letters(@game, 'ogd')
       expect(@game.check_win_or_lose).to eq(:win)
     end
-    it 'should be lose after 7 incorrect guesses', :pending => true do
+    it 'should be lose after 7 incorrect guesses' do
       guess_several_letters(@game, 'tuvwxyz')
       expect(@game.check_win_or_lose).to eq(:lose)
     end
-    it 'should continue play if neither win nor lose', :pending => true do
+    it 'should continue play if neither win nor lose' do
       guess_several_letters(@game, 'do')
       expect(@game.check_win_or_lose).to eq(:play)
     end
